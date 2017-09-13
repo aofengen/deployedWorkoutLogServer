f72f796f0421e862eecdd84feb0e2f28f5332bb4
Aaron Ofengender <aofengen@gmail.com>
2017-09-13 16:16:51 -0400

    hotfix

--- db.js
+++ db.js
@@ -1,7 +1,6 @@
 const Sequelize = require('sequelize');
 let sequelize = new Sequelize(process.env.DATABASE_URL || '//postgres://postgres:9074dewberry1136@localhost:5432/workoutlog',{
 	dialect: 'postgres'
-});
 
 // let sequelize = new Sequelize('workoutlog','postgres','9074dewberry1136',{
 // 	host: 'localhost',
